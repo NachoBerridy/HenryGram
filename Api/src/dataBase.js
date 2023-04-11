e51f28ff73f12e632d918d208d79e162307d8839
Nachoberridy <nachoberridy1997@gmail.com>
2023-04-11 18:51:18 -0300

    secret uri

--- Api/src/dataBase.js
+++ Api/src/dataBase.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
 const MONGODB_URI = process.env.URI //|| 'mongodb://localhost/test-app';
-// mongodb+srv://HenryGram:HeGr2022@henrygram.xbooldv.mongodb.net/test
-// mongodb+srv://HenryGram:HeGr2022@henrygram.xbooldv.mongodb.net/?retryWrites=true&w=majority
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
